bf43e488ac337353a26590b59f1b25d3df1b2fcf
sanwan000 <448878898@qq.com>
2014-11-04 17:23:56 +0800

    a

---
+++ fragment/src/com/example/fragment/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import com.example.fragment.MovieFragment;
+import com.example.fragment.MusicFragment;
+import com.example.fragment.TVFragment;
+
+public class MainActivity extends FragmentActivity implements OnClickListener {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
